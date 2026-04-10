23c48c7565df089dc8d27c006dcf308fd97e846d
Deepti_Mishra <140061405+DEEPTI173@users.noreply.github.com>
2026-04-10 22:08:38 +0530

    Update grader.py

--- grader.py
+++ grader.py
@@ -1,18 +1,12 @@
 # grader.py
 
-def grade_easy(output: str) -> float:
-    if not output:
-        return 0.2
-    return 0.4   # easy score
+def grade_easy(input_data, output_data):
+    return 0.4  # valid (0 < score < 1)
 
 
-def grade_medium(output: str) -> float:
-    if not output:
-        return 0.3
-    return 0.6   # medium score
+def grade_medium(input_data, output_data):
+    return 0.6  # valid
 
 
-def grade_hard(output: str) -> float:
-    if not output:
-        return 0.4
-    return 0.8   # hard score
+def grade_hard(input_data, output_data):
+    return 0.8  # valid
